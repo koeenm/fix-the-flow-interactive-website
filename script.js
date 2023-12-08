4bf34799806551376ef7e6fc08b74c7e225eb905
koeenm <mkoeen2@gmail.com>
2023-12-08 09:52:41 +0100

    fallback

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const helpBtn = document.getElementById('help-button');
 const closeBtn = document.getElementById('close');
 
 const nextBtn = document.getElementById('next');
-const backBtn = document.getElementById('back')
+const backBtn = document.getElementById('back');
 
 const stepGuide = document.getElementById('step-guide')
-const dialog = document.querySelector("dialog");
+const step1 = document.getElementById("step1");
+// const step2 = document.getElementById("step2");
+// const step3 = document.getElementById("step3");
 
 
 helpBtn.addEventListener("click", function() {
-    dialog.showModal();
-  });
+    step1.showModal();
+});
 
-  closeBtn.addEventListener("click", function() {
-    dialog.close();
-  });
+closeBtn.addEventListener("click", function() {
+    step1.close();
+});
+
+nextBtn.addEventListener("click", function() {
+    step2.showModal();
+});
+
