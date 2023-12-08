b87cf6ae38a9623f7912b5e39443898c3cc21bcc
koeenm <mkoeen2@gmail.com>
2023-12-08 12:31:43 +0100

    responsive

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const helpBtn = document.getElementById('help-button');
-const closeBtn = document.getElementById('close');
+const helpBtn = document.querySelector('#help-button');
+const closeBtn = document.querySelector('#close');
 
-const nextBtn = document.getElementById('next');
-const backBtn = document.getElementById('back');
+const nextBtn = document.querySelector('#next');
+// const backBtn = document.getElementById('back');
 
-const stepGuide = document.getElementById('step-guide')
-const step1 = document.getElementById("step1");
+const stepGuide = document.querySelector('#step-guide')
+const step1 = document.querySelector("#step1");
 // const step2 = document.getElementById("step2");
 // const step3 = document.getElementById("step3");
 
@@ -18,7 +18,5 @@ closeBtn.addEventListener("click", function() {
     step1.close();
 });
 
-nextBtn.addEventListener("click", function() {
-    step2.showModal();
-});
+
 
